cf20e6144c8bc56f0314661e973eccb8edd56c1a
Alex P. Andrade <g.alex.andrade@gmail.com>
2018-10-01 22:29:08 -0300

    Axios instances

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import registerServiceWorker from './registerServiceWorker';
 import axios from 'axios';
 
 axios.defaults.baseURL = 'https://jsonplaceholder.typicode.com';
-axios.defaults.headers.common['Authorization'] = 'AUTH TOKEN';
+axios.defaults.headers.common['Authorization'] = 'AUTH TOKEN'
 
 
 axios.interceptors.request.use(request => {
