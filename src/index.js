a51fb022e0da290e31aca2d7c3522b3ca2048d3c
Alex P. Andrade <g.alex.andrade@gmail.com>
2018-10-01 22:15:08 -0300

    Interceptors

--- src/index.js
+++ src/index.js
@@ -3,6 +3,30 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import axios from 'axios';
+
+axios.interceptors.request.use(request => {
+    console.log(request);
+    //EDIT REQUEST
+    return request;
+},
+//Only errors related to the request (internet ussues, etc)
+error => {
+    console.log(error);
+    return Promise.reject(error);
+});
+
+
+axios.interceptors.response.use(response => {
+    console.log(response);
+    //EDIT RESPONSE
+    return response;
+},
+//Only errors related to the request (internet ussues, etc)
+error => {
+    console.log(error);
+    return Promise.reject(error);
+});
 
 ReactDOM.render( <App />, document.getElementById( 'root' ) );
 registerServiceWorker();
