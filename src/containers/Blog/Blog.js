5827b30d7ad1bbb13da4ff89199cb14dfef42d11
Alex Andrade <g.alex.andrade@gmail.com>
2018-10-03 07:41:13 -0300

    Switch

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 //import axios from 'axios';
-import { Route, NavLink } from 'react-router-dom';
+import { Route, NavLink, Switch } from 'react-router-dom';
 
 import NewPost from './NewPost/NewPost';
 import FullPost from './FullPost/FullPost';
@@ -37,9 +37,12 @@ class Blog extends Component {
                 <Route path="/" exact render={() => <h1>Home 2</h1>}/>
                 */}
 
-                <Route path="/" exact component={Posts}/>
-                <Route path="/new-post" component={NewPost}/>
-                <Route path="/:id" exact component={FullPost}/>
+                <Switch>
+                    <Route path="/" exact component={Posts}/>
+                    <Route path="/new-post" component={NewPost}/>
+                    <Route path="/:id" exact component={FullPost}/>
+                </Switch>
+                
             </div>
         );
     }
