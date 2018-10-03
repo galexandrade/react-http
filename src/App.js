2340b16e898eeed1fb4a4740ae3d996304fbd37c
Alex Andrade <g.alex.andrade@gmail.com>
2018-10-03 08:56:36 -0300

    Base path

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import Blog from './containers/Blog/Blog';
 class App extends Component {
   render() {
     return (
-      <BrowserRouter>
+      <BrowserRouter basename="/">
         <div className="App">        
           <Blog />
         </div>      
