c9f6790bafe383f56e870f7a863e8159ee836f0e
Alex Andrade <g.alex.andrade@gmail.com>
2018-10-03 07:45:30 -0300

    Navigate programaticaly

--- src/containers/Blog/Posts/Posts.js
+++ src/containers/Blog/Posts/Posts.js
@@ -33,9 +33,8 @@ class Posts extends Component{
     }
 
     postSelectedHandler = (id) => {
-        this.setState({
-            selectedPostId: id
-        });
+        //this.props.history.push({pathname: '/' + id});
+        this.props.history.push('/' + id);
     }
 
     render(){
@@ -43,12 +42,13 @@ class Posts extends Component{
 
         if(!this.state.error){
             posts = this.state.posts.map(post => (
-                <Link to={'/' + post.id} key={post.id}>
+                //<Link to={'/' + post.id} key={post.id}>
                     <Post
+                        key={post.id}
                         title={post.title}
                         author={post.author}
                         clicked={() => this.postSelectedHandler(post.id)}/>
-                </Link>
+                //</Link>
             ));
         }
 
